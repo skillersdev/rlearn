dc0689ccbdbf9d00076dc6d9c73d58b3b17d5601
sridhar <sridhar.muthusamy@signzy.com>
2023-01-09 18:18:59 +0530

    localdb

--- elearningsite/application/config/database.php
+++ elearningsite/application/config/database.php
@@ -73,27 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'thrabcse_3abc7',
-// 	'password' => '3abc7123!@#!@#',
-// 	'database' => 'thrabcse_new',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => 'elearn_',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
+
 
 
 $db['default'] = array(
